3207dddc3f3566cf867938312bfc843ac23211c3
reteee <104433082+reteee@users.noreply.github.com>
2022-06-12 14:01:27 +0700

    Update chat_status.py

--- TomoeRobot/modules/helper_funcs/chat_status.py
+++ TomoeRobot/modules/helper_funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from SkyzuRobot.modules import connection
+from TomoeRobot.modules import connection
 
 connected = connection.connected
