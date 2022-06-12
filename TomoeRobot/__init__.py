1b583774e609fe9fec94fc6f0caa473574d50e39
reteee <104433082+reteee@users.noreply.github.com>
2022-06-12 13:55:45 +0700

    Update __init__.py

--- TomoeRobot/__init__.py
+++ TomoeRobot/__init__.py
@@ -292,7 +292,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from SkyzuRobot.modules.helper_funcs.handlers import (
+from TomoeRobot.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
